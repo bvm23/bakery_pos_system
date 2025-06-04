66c737954de27d857d5f89d302a9b8e1691ba4da
bvm23 <bezalelvm17@gmail.com>
2025-06-04 22:36:10 +0530

    inter font and basic routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PointOfSalesComponent } from './features/point-of-sales/point-of-sales.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/pointofsales',
+    pathMatch: 'full',
+  },
+  { path: 'pointofsales', component: PointOfSalesComponent },
+];
---
+++ src/app/features/point-of-sales/point-of-sales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'b-point-of-sales',
+  imports: [],
+  templateUrl: './point-of-sales.component.html',
+  styleUrl: './point-of-sales.component.scss'
+})
+export class PointOfSalesComponent {
+
+}
